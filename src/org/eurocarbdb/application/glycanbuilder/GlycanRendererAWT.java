d36ef82072c4d244a8f7d4c39b66488872622897
David Damerell (david <David Damerell (david@nixbioinf.org)>
2011-02-07 15:50:36 +0000

    Added constructor which takes an instance of AbstractGlycanRenderer|Clones basic settings from the passed GlycanRenderer.

--- src/org/eurocarbdb/application/glycanbuilder/GlycanRendererAWT.java
+++ src/org/eurocarbdb/application/glycanbuilder/GlycanRendererAWT.java
@@ -58,6 +58,10 @@ public class GlycanRendererAWT extends AbstractGlycanRenderer {
 	public GlycanRendererAWT() {
 		super();
 	}
+	
+	public GlycanRendererAWT(AbstractGlycanRenderer src) {
+		super(src);
+	}
 
 	public GlycanRendererAWT(GlycanRendererAWT src) {
 		super(src);

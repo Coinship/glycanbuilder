1536aadcd825a9e52729e43208036220a4aac51a
David Damerell <David Damerell@localhost>
2011-06-14 16:04:26 +0100

    Missing from previous commit

---
+++ VaadinGlycanBuilder/src/ac/uk/icl/dell/vaadin/SimpleFileMenu.java
@@ -0,0 +1,63 @@
+/*
+*   EuroCarbDB, a framework for carbohydrate bioinformatics
+*
+*   Copyright (c) 2006-2011, Eurocarb project, or third-party contributors as
+*   indicated by the @author tags or express copyright attribution
+*   statements applied by the authors.  
+*
+*   This copyrighted material is made available to anyone wishing to use, modify,
+*   copy, or redistribute it subject to the terms and conditions of the GNU
+*   Lesser General Public License, as published by the Free Software Foundation.
+*   A copy of this license accompanies this distribution in the file LICENSE.txt.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+*   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+*   for more details.
+*   
+*   @author David R. Damerell (david@nixbioinf.org)
+*/
+package ac.uk.icl.dell.vaadin;
+
+import ac.uk.icl.dell.vaadin.menu.ApplicationMenu;
+import ac.uk.icl.dell.vaadin.menu.DynamicMenuImpl;
+
+import com.vaadin.ui.MenuBar;
+
+public class SimpleFileMenu extends DynamicMenuImpl implements ApplicationMenu{
+	private static final long serialVersionUID=4968428219495753712L;
+	
+	private MenuItem fileMenuItem;
+	private MenuItem editMenuItem;
+	private MenuItem viewMenuItem;
+	
+	public SimpleFileMenu(){
+		fileMenuItem=addItem("File",null);
+		editMenuItem=addItem("Edit",null);
+		viewMenuItem=addItem("View",null);
+		
+		fileMenuItem.setVisible(false);
+		editMenuItem.setVisible(false);
+		viewMenuItem.setVisible(false);
+	}
+
+	@Override
+	public MenuBar.MenuItem getFileMenu(){
+		return fileMenuItem;
+	}
+
+	@Override
+	public MenuItem getEditMenu() {
+		return editMenuItem;
+	}
+
+	@Override
+	public MenuItem getViewMenu() {
+		return viewMenuItem;
+	}
+
+	@Override
+	public MenuBar getMenuBar() {
+		return this;
+	}
+}

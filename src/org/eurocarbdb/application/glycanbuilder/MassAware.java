f2e66e8b5c04f044954f188c6cc6348c07457a5b
David Damerell <David Damerell@localhost>
2011-09-29 16:54:30 +0100

    Minor updates to MassAware and Glycan

--- src/org/eurocarbdb/application/glycanbuilder/MassAware.java
+++ src/org/eurocarbdb/application/glycanbuilder/MassAware.java
@@ -2,4 +2,6 @@ package org.eurocarbdb.application.glycanbuilder;
 
 public interface MassAware{
 	public double computeMass();
+	public double computeMass(String type);
+	public boolean equals(MassAware aware);
 }
